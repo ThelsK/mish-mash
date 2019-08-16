af4c573667baa3ef434aeb7ee0811d26b7a128dd
ThelsK <github@thels.nl>
2019-08-16 21:41:28 +0200

    Implemented ShoppingCart

---
+++ src/ShoppingCart.js
@@ -0,0 +1,15 @@
+class ShoppingCart {
+    constructor() {
+        this.items = []
+    }
+    addItem = (name, quantity, pricePerUnit) => 
+        this.items.push({name, quantity, pricePerUnit})
+    getItems = () =>
+        this.items
+    clear = () =>
+        this.items = []
+    total = () =>
+        this.items.reduce((result, item) => result + item.quantity * item.pricePerUnit, 0)
+}
+
+module.exports = ShoppingCart
